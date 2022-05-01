6ed894786f123bf1b76bd4f43f039778cd323d4c
niracler <niracler@gmail.com>
2022-05-01 10:51:54 +0800

    chore(start&deploy): use x-cmd script to deploy and start

--- .x-cmd/docker/entrypoint.sh
+++ .x-cmd/docker/entrypoint.sh
@@ -1,4 +1,4 @@
 # shellcheck shell=sh
 
 export https_proxy=http://me.niracler.com:1080 http_proxy=http://me.niracler.com:1080 all_proxy=socks5://me.niracler.com:1080
-python bot.py "$1"
+python bot "$1"
