2dc0a629566655e3a2d1ca3f3bdcd2a3f50eb80d
niracler <niracler@gmail.com>
2022-06-22 21:56:26 +0800

    update proxy envirment

--- .x-cmd/docker/entrypoint.sh
+++ .x-cmd/docker/entrypoint.sh
@@ -1,4 +1,4 @@
 # shellcheck shell=sh
 
-export https_proxy=http://me.niracler.com:1080 http_proxy=http://me.niracler.com:1080 all_proxy=socks5://me.niracler.com:1080
+export https_proxy=http://me.niracler.com:2087 http_proxy=http://me.niracler.com:2087 all_proxy=socks5://me.niracler.com:2080
 python bot "$1"
